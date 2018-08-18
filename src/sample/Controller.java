a7d092d1f971ffe15149736800d49866018f0fd7
ali-ghazi78 <39215015+ali-ghazi78@users.noreply.github.com>
2018-08-18 21:47:22 +0430

    checkbox added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,37 +1,59 @@
 package sample;
 
 
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
 
-import javax.swing.*;
 import java.io.*;
-import java.util.Scanner;
 
 
 public class Controller {
     public TextField txt_bit;
     public TextField txt_elf;
     public TextField txt_bmm;
+    public CheckBox check_box_bmm;
+    public CheckBox check_box_bit;
+    public CheckBox check_box_elf;
+
     private int a=1;
     public void initialize() throws IOException {
         if(a==1) {
             //JOptionPane.showMessageDialog(null, "initilize", "a", JOptionPane.WARNING_MESSAGE);
 
             String path_read=this.read_path("bmm_path.text");
-            txt_bmm.setText(path_read);
+            if(path_read!=null)
+                txt_bmm.setText(path_read);
+            else
+                txt_bmm.setText("please open a file");
             path_read=this.read_path("elf_path.text");
-            txt_elf.setText(path_read);
-            path_read=this.read_path("bit_path.text");
-            txt_bit.setText(path_read);
+            if(path_read!=null)
+                txt_elf.setText(path_read);
+            else
+                txt_elf.setText("please open a file");
 
+            path_read=this.read_path("bit_path.text");
+            if(path_read!=null)
+                txt_bit.setText(path_read);
+            else
+                txt_bit.setText("please open a file");
             a=10;
         }
+        if(!txt_bit.getText().equals("please open a file")){
+            check_box_bit.setSelected(false);
+        }
+        if(!txt_elf.getText().equals("please open a file")){
+            check_box_elf.setSelected(false);
+        }
+        if(!txt_bmm.getText().equals("please open a file")){
+            check_box_bmm.setSelected(false);
+
+        }
     }
-    public void brows_bit(){
+    public void brows_bit() throws IOException {
         FileChooser ff=new FileChooser();
         Stage stage=new Stage();
         ff.setTitle("select bit file");
@@ -39,11 +61,18 @@ public class Controller {
         ff.getExtensionFilters().add(myFilter);
 
         File file=ff.showOpenDialog(stage);
-        if(file!=null)
+        if(file!=null) {
             txt_bit.setText(file.getPath());
+            FileWriter fileWriterPath=new FileWriter("bit_path.text");
+            PrintWriter printWriter=new PrintWriter(fileWriterPath);
+            printWriter.print(file.getPath());
+            printWriter.close();
+
+
+        }
 
     }
-    public void brows_elf() {
+    public void brows_elf() throws IOException {
         FileChooser ff=new FileChooser();
         Stage stage = new Stage();
         ff.setTitle("select elf file");
@@ -51,17 +80,21 @@ public class Controller {
         FileChooser.ExtensionFilter myFilter=new FileChooser.ExtensionFilter("ELF(*.elf)","*.elf");
         ff.getExtensionFilters().addAll(myFilter);
 
-        File elf_file=ff.showOpenDialog(stage);
+        File file=ff.showOpenDialog(stage);
+        if(file!=null) {
+            txt_elf.setText(file.getPath());
+            FileWriter fileWriterPath=new FileWriter("elf_path.text");
+            PrintWriter printWriter=new PrintWriter(fileWriterPath);
+            printWriter.print(file.getPath());
+            printWriter.close();
 
-        if(elf_file!=null)
-            txt_elf.setText(elf_file.getPath());
+
+        }
     }
 
     public void brows_bmm( ) throws IOException {
         FileChooser ff=new FileChooser();
         Stage stage=new Stage();
-
-        String path_read=this.read_path("bmm_path.text");
 
         ff.setTitle("select bmm file");
         FileChooser.ExtensionFilter myFilter=new FileChooser.ExtensionFilter("BMM(*.bmm)","*.bmm");
