456f652e1b86e01ea1dabe1887cac594d8a285fe
ali-ghazi78 <39215015+ali-ghazi78@users.noreply.github.com>
2018-08-18 20:54:02 +0430

    z

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,31 +1,37 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
+
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
+
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 import javax.swing.*;
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.security.PublicKey;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class Controller {
     public TextField txt_bit;
     public TextField txt_elf;
     public TextField txt_bmm;
+    private int a=1;
+    public void initialize() throws IOException {
+        if(a==1) {
+            //JOptionPane.showMessageDialog(null, "initilize", "a", JOptionPane.WARNING_MESSAGE);
 
+            String path_read=this.read_path("bmm_path.text");
+            txt_bmm.setText(path_read);
+            path_read=this.read_path("elf_path.text");
+            txt_elf.setText(path_read);
+            path_read=this.read_path("bit_path.text");
+            txt_bit.setText(path_read);
 
-    public void brows_bit(MouseEvent mouseEvent){
+            a=10;
+        }
+    }
+    public void brows_bit(){
         FileChooser ff=new FileChooser();
         Stage stage=new Stage();
         ff.setTitle("select bit file");
@@ -37,8 +43,7 @@ public class Controller {
             txt_bit.setText(file.getPath());
 
     }
-    Scene scene=null;
-    public void brows_elf(MouseEvent mouseEvent) throws IOException {
+    public void brows_elf() {
         FileChooser ff=new FileChooser();
         Stage stage = new Stage();
         ff.setTitle("select elf file");
@@ -52,16 +57,41 @@ public class Controller {
             txt_elf.setText(elf_file.getPath());
     }
 
-    public void brows_bmm(MouseEvent mouseEvent) {
+    public void brows_bmm( ) throws IOException {
         FileChooser ff=new FileChooser();
         Stage stage=new Stage();
+
+        String path_read=this.read_path("bmm_path.text");
 
         ff.setTitle("select bmm file");
         FileChooser.ExtensionFilter myFilter=new FileChooser.ExtensionFilter("BMM(*.bmm)","*.bmm");
         ff.getExtensionFilters().addAll(myFilter);
         File file=ff.showOpenDialog(stage);
-        if(file!=null)
+        if(file!=null){
             txt_bmm.setText(file.getPath());
+            FileWriter fileWriterPath=new FileWriter("bmm_path.text");
+            PrintWriter printWriter=new PrintWriter(fileWriterPath);
+            printWriter.print(file.getPath());
+            printWriter.close();
+        }
+        else{
+
+        }
+    }
+    public String read_path(String path) throws IOException {
+        File p=new File(path);
+
+        if(!p.exists()) {
+            JOptionPane.showMessageDialog(null,"incorrect path file","not found", JOptionPane.ERROR_MESSAGE);
+        }
+        else {
+            BufferedReader f = new BufferedReader(new FileReader(path));
+            String outputString;
+            outputString = f.readLine() ;
+            f.close();
+            return outputString;
+        }
+        return null;
 
 
     }
